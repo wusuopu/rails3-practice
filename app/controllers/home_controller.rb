ea4679b8f673ca0c2940462d41fbd950378d7105
wusuopu <admin@longchangjin.cn>
2015-03-31 22:39:46 +0800

    Create user role.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,10 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    auth = can? :manage, Role
+    puts "can manage? #{auth}"
+    puts "user_signed_in? #{user_signed_in?}"
+    puts "current_user: #{current_user}"
+    puts "user_session: #{user_session}"
   end
 end
