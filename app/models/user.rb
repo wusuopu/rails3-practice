25e013a9dfa74285168422e58ca143cd91a24b60
wusuopu <admin@longchangjin.cn>
2015-04-04 21:44:47 +0800

    Test model event callback.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,15 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
   attr_accessible :email, :name
   has_many :microposts
+
+  # 操作回调函数
+  after_save :update_action
+  after_destroy :destroy_action
+
+  def update_action
+    puts "#{self.id} has updated.."
+  end
+  def destroy_action
+    puts "#{self.id} has destroyed.."
+  end
 end
